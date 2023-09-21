56d425b4509a06ff683b7639c7c55d08f8bdd03d
amelieboydshire <aboydshire10@gmail.com>
2023-09-21 10:27:27 +0100

    initialised DepartmentService

--- capstone/src/main/java/com/bnta/capstone/repositories/DepartmentRepository.java
+++ capstone/src/main/java/com/bnta/capstone/repositories/DepartmentRepository.java
@@ -1,4 +1,10 @@
 package com.bnta.capstone.repositories;
 
-public class DepartmentRepository {
+
+import com.bnta.capstone.models.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DepartmentRepository extends JpaRepository<Department, Integer> {
 }
--- capstone/src/main/java/com/bnta/capstone/services/DepartmentService.java
+++ capstone/src/main/java/com/bnta/capstone/services/DepartmentService.java
@@ -1,4 +1,30 @@
 package com.bnta.capstone.services;
 
+import com.bnta.capstone.models.Department;
+import com.bnta.capstone.models.Employee;
+import com.bnta.capstone.repositories.DepartmentRepository;
+import com.bnta.capstone.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepartmentService {
+
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee findLeadEmployee(int id){
+        return this.employeeRepository.findById(id).get();
+    }
+
+    public Department findDepartmentName(int id){
+        return this.departmentRepository.findById(id).get();
+    }
+
+    
 }
