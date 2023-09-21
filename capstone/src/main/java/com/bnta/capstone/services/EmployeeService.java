dbeecf93943b7cf60fd7dc6aeff7b635d984552f
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-09-21 10:50:54 +0100

    get methods added to the employee service

--- capstone/src/main/java/com/bnta/capstone/services/EmployeeService.java
+++ capstone/src/main/java/com/bnta/capstone/services/EmployeeService.java
@@ -1,11 +1,36 @@
 package com.bnta.capstone.services;
 
+import com.bnta.capstone.models.Employee;
+import com.bnta.capstone.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     // get all employees
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+
     // get employee by id
+    public Employee getEmployeeById(int id){
+        return employeeRepository.findById(id).get();
+    }
+
+
+
     // get employees by department
 
+//    public List<Employee> getEmployeeByDepartment(){
+//       // find by department name or id??
+//    }
 
-    // save employee
+
 }
