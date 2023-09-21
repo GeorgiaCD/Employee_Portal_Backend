23757927e39577f6b66188ace53c1946b1814fac
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-09-21 10:43:02 +0100

    added sudo code to employee service

--- capstone/src/main/java/com/bnta/capstone/services/EmployeeService.java
+++ capstone/src/main/java/com/bnta/capstone/services/EmployeeService.java
@@ -2,5 +2,10 @@ package com.bnta.capstone.services;
 
 public class EmployeeService {
 
-    
+    // get all employees
+    // get employee by id
+    // get employees by department
+
+
+    // save employee
 }
