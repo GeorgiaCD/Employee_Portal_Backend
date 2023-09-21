0d68a215447afb7ce30f87d84b6a6957141d867c
Francesca Marshall <francescam3105@gmail.com>
2023-09-21 10:59:08 +0100

    created employee Service

--- capstone/src/main/java/com/bnta/capstone/controllers/EmployeeController.java
+++ capstone/src/main/java/com/bnta/capstone/controllers/EmployeeController.java
@@ -1,4 +1,37 @@
 package com.bnta.capstone.controllers;
 
+import com.bnta.capstone.models.Employee;
+import com.bnta.capstone.services.EmployeeService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getEmployees(){
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id){
+        Employee employee = employeeService.getEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+
+
 }
