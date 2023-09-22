c6f2769db425f7cc86ad738cdee0e47b8f15738b
Francesca Marshall <francescam3105@gmail.com>
2023-09-22 15:18:05 +0100

    added LoginDTO

---
+++ src/main/java/com/bnta/capstone/models/LoginDTO.java
@@ -0,0 +1,36 @@
+package com.bnta.capstone.models;
+
+public class LoginDTO {
+
+    private String email;
+
+    private String password;
+
+    public LoginDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public LoginDTO() {
+    }
+
+//    Getters & Setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
