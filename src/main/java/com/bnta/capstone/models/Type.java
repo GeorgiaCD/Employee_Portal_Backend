6db69c1ba4aff320e11d7ffd28e673186472ffc9
blezzydcruz <mbannedelacruz@hotmail.com>
2023-09-25 09:38:57 +0100

    added start time to Type enum

--- src/main/java/com/bnta/capstone/models/Type.java
+++ src/main/java/com/bnta/capstone/models/Type.java
@@ -2,7 +2,24 @@ package com.bnta.capstone.models;
 
 public enum Type {
 
-    MORNING,
-    AFTERNOON,
-    EVENING
+    MORNING_9AM("9am"),
+    MORNING_11AM ("11am"),
+    AFTERNOON_1PM ("1pm"),
+    AFTERNOON_4PM ("4pm"),
+    EVENING_6PM ("6pm"),
+    EVENING_9PM("9pm");
+
+    private String startTime;
+
+    Type(String startTime) {
+        this.startTime = startTime;
+    }
+    
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
 }
